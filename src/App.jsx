628f4500c69465e7fd11510a2269a327d7561592
vivekrai1999 <vivek.rai.2807@gmail.com>
2024-01-21 10:30:15 +0530

    Added Sorting

--- src/App.jsx
+++ src/App.jsx
@@ -62,10 +62,22 @@ function Form({onAddItem}){
 }
 
 function PackingList({items, onDelete, onChecked}){
+  const [sortBy, setSortBy] = useState('input')
+  let sortedItems;
+  if(sortBy==='input') sortedItems = items;
+  if(sortBy==='description') sortedItems = items.slice().sort((a,b)=>a.description.localeCompare(b.description))
+  if(sortBy==='packed') sortedItems = items.slice().sort((a,b)=>Number(a.packed)-Number(b.packed))
   return <div className="list">
   <ul>
-    {items.map(item=><Item key={item.id} item={item} onDelete={onDelete} onChecked={onChecked}/>)}
+    {sortedItems.map(item=><Item key={item.id} item={item} onDelete={onDelete} onChecked={onChecked}/>)}
   </ul>
+  <div className="actions">
+      <select value={sortBy} onChange={(e)=>setSortBy(e.target.value)}>
+        <option value="input">Sort by input order</option>
+        <option value="description">sort by description</option>
+        <option value="packed">sort by packed status</option>
+      </select>
+    </div>
   </div>
 }
 
